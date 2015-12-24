fdf6b533284dc23876d76dcecb646f6bbef78a63
AxeAdam <adamarko@gmail.com>
2015-12-24 15:39:32 +0100

    work in progress

--- product_page_template.php
+++ product_page_template.php
@@ -29,16 +29,40 @@
     <p class="path"><?php echo getcwd() . "\n" . "{{IME APARATA}}"; ?></p>
     <div class="product">
         <div class="porTumb">
-
+            <img src="img/product/1.jpg"/>
+            <img src="img/product/2.jpg"/>
+            <img src="img/product/3.jpg"/>
+            <img src="img/product/4.jpg"/>
         </div>
         <div class="mainImg">
-            <img />
+            <img src="img/product/1.jpg"/>
             <div class="share">
-
+                <button class="compare">COMPARE</button>
+                <button class="share">SHARE</button>
             </div>
         </div>
         <div class="specPrac">
-
+            <img src="img/brands/gorenje.png" class="brand"/>
+            <p class="name">Gorenje washing machines W62S2/Y 6Kg</p>
+            <!-- TODO: Reviews and score -->
+            <ul class="prodInfo">
+                <li>Whisper quiet motor that's also longer lasting</li>
+                <li>Textured drum takes extra care of clothes</li>
+                <li>Allergy setting, great for sensitive skin</li>
+                <li>Pick a wash that's 60% quicker or saves 30% energy</li>
+                <li>Dimensions (cm) - H85 x W60 x D55</li>
+                <!-- TODO: Read full and show less -->
+                <p>Read full product information</p>
+                <table class="price">
+                <tr>
+                    <td rowspan="2" class="new">499&euro;</td>
+                    <td class="old">Old price: 599&euro;</td>
+                </tr>
+                <tr>
+                    <td class="save">Save: 100&euro;</td>
+                </tr>
+                </table>
+            </ul>
         </div>
     </div>
 </body>
