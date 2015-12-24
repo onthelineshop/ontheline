1c9aa61161caa0e9570b8ad2676eb7f62d32b45f
AxeAdam <adamarko@gmail.com>
2015-12-24 11:34:24 +0100

    Started

---
+++ product_page_template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{{IME APARATA}}</title>
+    <link href="/css/bootstrap.min.css" type="text/css" />
+    <link href="/css/bootstrap-theme.min.css" type="text/css" />
+    <!-- CSS for product pages -->
+    <link rel="stylesheet" href="/css/product.css" type="text/css" />
+    </head>
+    <body>
+        <div class="head">
+            <img src="img/head.png" />
+        </div>
+    </body>
+</html>
